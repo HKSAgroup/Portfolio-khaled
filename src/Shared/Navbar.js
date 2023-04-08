11cf9e20d278bff40df88f48a5783ec8295f68aa
Md Imran Hossen <freelancerimran81@gmail.com>
2023-04-08 20:46:06 +0600

    menu slv

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -8,6 +8,7 @@ import { MdOutlineDashboard } from 'react-icons/md';
 const Navbar = () => {
   const router = useRouter();
   const { pathname } = useRouter();
+  const [tabOpen, setTabOpen] = useState(false);
 
 
   return (
@@ -18,33 +19,38 @@ const Navbar = () => {
 
             <div className="navbar-start ">
 
-            <div className="dropdown">
-                <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <div className="dropdown">
+                <label
+                  onClick={() => setTabOpen(!tabOpen)}
+                  tabIndex={0} className="btn btn-ghost lg:hidden">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </label>
 
 
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                <Link href='/'>
-                    <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Home</h6></li>
-                  </Link>
-                  <Link href='/about'>
-                    <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>About</h6></li>
-                  </Link>
-                  <Link href='/service'>
-                    <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Service</h6></li>
-                  </Link>
-                  <Link href='/blog'>
-                    <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Blog</h6></li>
-                  </Link>
-                  <Link href='/contact'>
-                    <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Contact</h6></li>
-                  </Link>
-                  <Link href='/payment'>
-                    <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Payment</h6></li>
-                  </Link>
-
-                </ul>
+                {
+                  tabOpen && (
+                    <ul className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                      <Link href='/'>
+                        <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Home</h6></li>
+                      </Link>
+                      <Link href='/about'>
+                        <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>About</h6></li>
+                      </Link>
+                      <Link href='/service'>
+                        <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Service</h6></li>
+                      </Link>
+                      <Link href='/blog'>
+                        <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Blog</h6></li>
+                      </Link>
+                      <Link href='/contact'>
+                        <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Contact</h6></li>
+                      </Link>
+                      <Link href='/payment'>
+                        <li><h6 className='font-medium text-[16px] hover:bg-base-100 hover:text-primary'>Payment</h6></li>
+                      </Link>
+                    </ul>
+                  )
+                }
               </div>
 
               <Link href='/' className="ml-3 flex justify-center items-center gap-2">
@@ -58,7 +64,7 @@ const Navbar = () => {
                 <h1 className="font-bold text-xl text-info">Portfolio</h1>
               </Link>
 
-              
+
 
             </div>
 
