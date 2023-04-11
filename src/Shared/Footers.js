80a8b0a4036ebda6f30c7c2a2215ca234c380bc6
Md Imran Hossen <freelancerimran81@gmail.com>
2023-04-11 11:10:48 +0600

    footer update

--- src/Shared/Footers.js
+++ src/Shared/Footers.js
@@ -8,9 +8,9 @@ const Footers = () => {
     <>
       <div className="bg-accent mt-10">
         <div className="mid-container">
-          <footer className="footer sm: pt-10  lg:pb-32 md:pb-28 sm:pb-16 pb-8 grid lg:grid-cols-5 md:grid-cols-4 grid-cols-2 gap-10">
-            <div>
-              <d iv className='flex items-center gap-2'>
+          <footer className=" sm:pt-20 pt-10 md:pb-28 sm:pb-16 pb-8 lg:flex  gap-14">
+            <div className='lg:w-[30%]'>
+              <div className='flex items-center gap-2'>
                 <Image
                   src={img}
                   alt="Picture of the author"
@@ -18,40 +18,42 @@ const Footers = () => {
                   height={120}
                 />
                 {/* <h1 className='font-semibold text-2xl'>Portfolio</h1> */}
-              </d>
-              <p className='text-justify text-sm text-neutral'>Professional designer developer and marketer. Working more then 2 thousand clients Globally. Main target provide best level services for business owner.</p>
-              <div className="grid grid-flow-col gap-4 mt-2 text-info">
+              </div>
+              <p className='text-justify text-sm text-neutral mt-2 mb-5'>Professional designer developer and marketer. Working more then 2 thousand clients Globally. Main target provide best level services for business owner.</p>
+              <div className="flex  items-center gap-4 text-info">
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer hover:text-primary duration-200"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path></svg></a>
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer hover:text-primary duration-200"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current cursor-pointer hover:text-primary duration-200"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
               </div>
             </div>
 
-            <div>
-              <span className="uppercase text-info font-semibold">Usefull Links</span>
-              <a className="link link-hover text-neutral">Home</a>
-              <a className="link link-hover text-neutral">Service</a>
-              <a className="link link-hover text-neutral">About Us</a>
-              <a className="link link-hover text-neutral">Project</a>
-            </div>
-            <div>
-              <span className="uppercase text-info font-semibold">Support</span>
-              <a className="link link-hover text-neutral">Contact</a>
-              <a className="link link-hover text-neutral">About</a>
-              <a className="link link-hover text-neutral">FAQs</a>
-              <a className="link link-hover text-neutral">Forms</a>
-            </div>
-            <div>
-              <span className="uppercase text-info font-semibold">More</span>
-              <a className="link link-hover text-neutral">Terms of use</a>
-              <a className="link link-hover text-neutral">Privacy policy</a>
-              <a className="link link-hover text-neutral">Cookie policy</a>
-            </div>
-            <div>
-              <span className="uppercase text-info font-semibold">Contact With Us</span>
-              <a className="link link-hover text-neutral">+8801722-570947</a>
-              <a className="link link-hover text-neutral">hello@khaledfullstack.com</a>
-              <a className="link link-hover text-neutral">Dhaka Bangladesh</a>
+            <div className='lg:w-[70%] grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:gap-0 gap-10 mt-10 lg:mt-0'>
+              <div className='space-y-4'>
+                <span className="uppercase text-info font-semibold">Usefull Links</span>
+                <div><a className="link link-hover text-neutral">Home</a></div>
+                <div><a className="link link-hover text-neutral">Services</a></div>
+                <div><a className="link link-hover text-neutral">About Us</a></div>
+                <div><a className="link link-hover text-neutral">About Project</a></div>
+              </div>
+              <div className='space-y-4'>
+                <span className="uppercase text-info font-semibold">Support</span>
+                <div><a className="link link-hover text-neutral">Contact</a></div>
+                <div><a className="link link-hover text-neutral">About</a></div>
+                <div><a className="link link-hover text-neutral">FAQ's</a></div>
+                <div><a className="link link-hover text-neutral">Forms</a></div>
+              </div>
+              <div className='space-y-4'>
+                <span className="uppercase text-info font-semibold">More</span>
+                <div><a className="link link-hover text-neutral">Terms of use</a></div>
+                <div><a className="link link-hover text-neutral">Privacy & Policy</a></div>
+                <div><a className="link link-hover text-neutral">Cookie & Policy</a></div>
+              </div>
+              <div className='space-y-4'>
+                <span className="uppercase text-info font-semibold">Contact With Us</span>
+                <div><a className="link link-hover text-neutral">+8801722-570947</a></div>
+                <div><a className="link link-hover text-neutral md:text-[13px] sm:text-[16px] text-xs">hello@khaledfullstack.com</a></div>
+                <div><a className="link link-hover text-neutral">Dhaka Bangladesh</a></div>
+              </div>
             </div>
           </footer>
 
